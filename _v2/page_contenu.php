3728051b4f4dcd266a4c9529d806dbaff7c78bae
Danzou62 <deuxdevolt@hotmail.com>
2014-03-14 16:05:06 +0100

    commit Lumith

--- _v2/page_contenu.php
+++ _v2/page_contenu.php
@@ -1,95 +1,180 @@
 <?php 
-	$nav_en_cours = 'other';
+	$nav_en_cours = 'home';
 	include('include_header.php');
 ?>
-      
-<div class="container" style=" padding-left: 0px; padding-right: 30px;">
-      <div class="row row-offcanvas row-offcanvas-right">
+    
+    <div class="container">      
+<div class="container">
 
-        <div class="col-xs-12 col-sm-9">
-          <p class="pull-right visible-xs">
-            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
-          </p>
-          <div class="jumbotron">
-            <h1>Hello, world!</h1>
-            <p>This is an example to show the potential of an offcanvas layout pattern in Bootstrap. Try some responsive-range viewport sizes to see it in action.</p>
-          </div>
-          <div class="row">
-      <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-          <img src="bootstrap/img/300x200.JPG" style="width: 300px; height: 200px;" data-src="holder.js/300x200" alt="300x200">
-          <div class="caption">
-            <h3>Thumbnail label</h3>
-            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-          </div>
+
+    <div class="well well-sm">
+        <strong>Category Title</strong>
+        <div class="btn-group">
+            <a href="http://localhost/My_Print3r/_v2/page_contenu.php#" id="list" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-th-list">
+            </span>List</a> <a href="http://localhost/My_Print3r/_v2/page_contenu.php#" id="grid" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-th"></span>Grid</a>
         </div>
-      </div>
-      <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-			<img src="bootstrap/img/300x200.JPG" style="width: 300px; height: 200px;" data-src="holder.js/300x200" alt="300x200">          <div class="caption">
-            <h3>Thumbnail label</h3>
-            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-			<img src="bootstrap/img/300x200.JPG" style="width: 300px; height: 200px;" data-src="holder.js/300x200" alt="300x200">          <div class="caption">
-            <h3>Thumbnail label</h3>
-            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-          </div>
-        </div>
-      </div>
     </div>
-	<div class="row">
-      <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-          <img src="bootstrap/img/300x200.JPG" style="width: 300px; height: 200px;" data-src="holder.js/300x200" alt="300x200">
-          <div class="caption">
-            <h3>Thumbnail label</h3>
-            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+	<div class="jumbotron">
+            <h1>Bienvenue sur la page contenu!</h1>
+            <p>Sur cette page vous pouver telecharger tout les contenus disponibles sur le site d'un simple clic.</p>
           </div>
+    <div id="products" class="row list-group">
+        <div class="item  col-xs-12 col-sm-8 col-md-6 col-lg-4">
+            <div class="thumbnail">
+                <img class="group list-group-image" src="bootstrap/img/fff.jpg" alt="">
+                <div class="caption" style="
+    padding-bottom: 0px;
+">
+                    <h4 class="group inner list-group-item-heading">
+                        Titre du modèle</h4>
+                    <p class="group inner list-group-item-text" style="margin-bottom: 20px;
+">
+                        Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                    <div class="row">
+                        <div class="col-xs-12 col-md-6">
+                            <div class="panel-footer text-center" style="width: 340px; height: 39px;">
+                    <a href="http://localhost/My_Print3r/_v2/page_contenu.php#download"><span class="glyphicon glyphicon-download" style="height: 22px; font-size: 22px;"></span></a>
+                    <a href="http://localhost/My_Print3r/_v2/page_contenu.php#facebook"><span class="glyphicon glyphicon-thumbs-up" style=" height: 22px; font-size: 22px; "></span></a>
+                    <a href="http://localhost/My_Print3r/_v2/page_contenu.php#twitter"><span class="glyphicon glyphicon-globe" style=" height: 22px; font-size: 22px; "></span></a>
+                    <a href="http://localhost/My_Print3r/_v2/page_contenu.php#share"><span class="glyphicon glyphicon-share-alt" style=" height: 22px; font-size: 22px; "></span></a>
+                </div><p></p>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-			<img src="bootstrap/img/300x200.JPG" style="width: 300px; height: 200px;" data-src="holder.js/300x200" alt="300x200">          <div class="caption">
-            <h3>Thumbnail label</h3>
-            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-          </div>
+        <div class="item  col-xs-12 col-sm-8 col-md-6 col-lg-4">
+            <div class="thumbnail">
+                <img class="group list-group-image" src="bootstrap/img/fff.jpg" alt="">
+                <div class="caption">
+                    <h4 class="group inner list-group-item-heading">
+                        Product title</h4>
+                    <p class="group inner list-group-item-text">
+                        Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                    <div class="row">
+                        <div class="col-xs-12 col-md-6">
+                            <p class="lead">
+                                $21.000</p>
+                        </div>
+                        <div class="col-xs-12 col-md-6">
+                            <a class="btn btn-success" href="http://www.jquery2dotnet.com/">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-			<img src="bootstrap/img/300x200.JPG" style="width: 300px; height: 200px;" data-src="holder.js/300x200" alt="300x200">          <div class="caption">
-            <h3>Thumbnail label</h3>
-            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-          </div>
+        <div class="item  col-xs-12 col-sm-8 col-md-6 col-lg-4">
+            <div class="thumbnail">
+                <img class="group list-group-image" src="bootstrap/img/fff.jpg" alt="">
+                <div class="caption">
+                    <h4 class="group inner list-group-item-heading">
+                        Product title</h4>
+                    <p class="group inner list-group-item-text">
+                        Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                    <div class="row">
+                        <div class="col-xs-12 col-md-6">
+                            <p class="lead">
+                                $21.000</p>
+                        </div>
+                        <div class="col-xs-12 col-md-6">
+                            <a class="btn btn-success" href="http://www.jquery2dotnet.com/">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
+        <div class="item  col-xs-12 col-sm-8 col-md-6 col-lg-4">
+            <div class="thumbnail">
+                <img class="group list-group-image" src="bootstrap/img/fff.jpg" alt="">
+                <div class="caption">
+                    <h4 class="group inner list-group-item-heading">
+                        Product title</h4>
+                    <p class="group inner list-group-item-text">
+                        Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                    <div class="row">
+                        <div class="col-xs-12 col-md-6">
+                            <p class="lead">
+                                $21.000</p>
+                        </div>
+                        <div class="col-xs-12 col-md-6">
+                            <a class="btn btn-success" href="http://www.jquery2dotnet.com/">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="item  col-xs-12 col-sm-8 col-md-6 col-lg-4">
+            <div class="thumbnail">
+                <img class="group list-group-image" src="bootstrap/img/fff.jpg" alt="">
+                <div class="caption">
+                    <h4 class="group inner list-group-item-heading">
+                        Product title</h4>
+                    <p class="group inner list-group-item-text">
+                        Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                    <div class="row">
+                        <div class="col-xs-12 col-md-6">
+                            <p class="lead">
+                                $21.000</p>
+                        </div>
+                        <div class="col-xs-12 col-md-6">
+                            <a class="btn btn-success" href="http://www.jquery2dotnet.com/">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="item  col-xs-12 col-sm-8 col-md-6 col-lg-4">
+            <div class="thumbnail">
+                <img class="group list-group-image" src="bootstrap/img/fff.jpg" alt="">
+                <div class="caption">
+                    <h4 class="group inner list-group-item-heading">
+                        Product title</h4>
+                    <p class="group inner list-group-item-text">
+                        Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                        sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                    <div class="row">
+                        <div class="col-xs-12 col-md-6">
+                            <p class="lead">
+                                $21.000</p>
+                        </div>
+                        <div class="col-xs-12 col-md-6">
+                            <a class="btn btn-success" href="http://www.jquery2dotnet.com/">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    </div><!--/span-->
+</div>
 
-        <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
-          <div class="list-group">
-            <a href="#" class="list-group-item active">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-            <a href="#" class="list-group-item">Link</a>
-          </div>
-        </div><!--/span-->
-      </div><!--/row-->
 
-<?php include("include_footer.php"); ?>
+</div>
+
+      <!-- FOOTER -->
+      <footer>
+        <p class="pull-right"><a href="http://localhost/My_Print3r/_v2/page_contenu.php#">^ Remonter ^</a></p>
+        <p>© 2014 Danzou62, Inc.</p>
+      </footer>
+
+    <!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./page_contenu.php_files/jquery.min.js"></script>
+    <script src="./page_contenu.php_files/bootstrap.min.js"></script>
+    <script src="./page_contenu.php_files/docs.min.js"></script>
+    <script src="./page_contenu.php_files/custom.js"></script>
+
+
+
+</body></html>
+
+<?php include('include_footer.php'); ?>
