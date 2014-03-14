c4e94d64204226272460d1ce9d2e47a84e606c55
Danzou62 <deuxdevolt@hotmail.com>
2014-03-14 23:59:44 +0100

    changement page_contenu

--- _v2/bootstrap/js/custom.js
+++ _v2/bootstrap/js/custom.js
@@ -7,3 +7,9 @@
       $(".pr-wrap").removeClass("show-pass-reset");
     }); 
 });
+
+
+$(document).ready(function() {
+    $('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
+    $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
+});
